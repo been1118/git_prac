538ca552f39d545b6ea2526639da04b53eded0be
SeokBin02 <ghkdtjrqls02@gmail.com>
2023-03-25 22:58:35 +0900

    Swtich 예제!!

---
+++ TNF.java
@@ -0,0 +1,7 @@
+public class TNF {
+    public static void main(String[] args) {
+        // switch문을 사용해서 푸세요
+        // 다음과 같이 and와 or의 논리 연산을 입력받아 결과를 출력하는 프로그램을 작성해보세요
+        // 예를들어 'true AND false'의 결과로 false를 'true OR false'의 결과로 true를 출력하면 됩니다.
+    }
+}
